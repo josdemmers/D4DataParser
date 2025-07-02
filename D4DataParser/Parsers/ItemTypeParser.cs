94a563de703ad0eafe81907d49c26e8c6bb622ba
Jos Demmers <8627157+josdemmers@users.noreply.github.com>
2025-07-02 17:12:28 +0200

    Removed sigil workaround

--- D4DataParser/Parsers/ItemTypeParser.cs
+++ D4DataParser/Parsers/ItemTypeParser.cs
@@ -273,9 +273,6 @@ namespace D4DataParser.Parsers
             {
                 string name = $"{RemoveVariantIndicator(itemTypeLoc)}".Trim();
 
-                // TODO: Temp fix because some languages haven't translated the Escalation Sigil and use Nightmare Sigil instead.
-                if (_itemTypeInfoList.Any(t => t.Name.Equals(name))) return;
-
                 _itemTypeInfoList.Add(new ItemTypeInfo
                 {
                     Name = name,
